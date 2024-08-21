fb00fd50ae6ea77a3434a8e1d7d3e48875f8160e
Sahus Gupta <sahusgupta@gmail.com>
2024-08-21 11:51:39 -0500

    ch1 and 2 done almost done w 3

---
+++ GD2_CH1to3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+/*
+ * public class GD2_CH1to3
+{
+    public static (float, float) Totals(float[] c)
+    {
+        float sub = 0;
+        foreach (float x in c)
+        {
+            sub += x;
+        }
+        (float, float) totals = (sub, sub * 1.0825f);
+        return totals;
+
+    }
+
+    public static (string[], string[], float, float) EvaluateScore ((string, int)[] studs)
+    {
+        string[] names = new string[studs.Length];
+        for (int i = 0; i < studs.Length; i++) {
+            names[i] = studs[i].Item1;
+        }
+        float passed = 0;
+        float total = 0;
+        bool[] pd = new bool[studs.Length];
+        foreach ((string, float) c in studs) {
+            passed += c.Item2 >= 3 ? 1 : 0;
+            total += c.Item2;
+        }
+        for (int i = 0; i < studs.Length; i++) {
+
+        (string[], string[], float, float) ret = (names, names, passed / studs.Length, total / studs.Length)
+    }
+}
+
+ */
